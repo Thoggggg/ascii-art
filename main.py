205bf58c3caa5c19f2998fe43bc104fa46f446c8
Thoggggg <garnier.theo92@gmail.com>
2024-04-05 15:33:03 +0200

    add --force-print

--- main.py
+++ main.py
@@ -7,15 +7,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument("word", help="Word to write")
 parser.add_argument("-s", "--symbol", dest = "symbol", default = "@", help="Symbol to write with")
 parser.add_argument("-o", "--output-file", dest = "output", default = "", help="Output file with the text to write")
+parser.add_argument("--force-print", help="If the file needs to be written in the command line", action='store_true')
 args = parser.parse_args()
 
 symbol = args.symbol
 word = args.word
 output = args.output
-verbose = False
-if output == "":
-    verbose = True
+verbose = (output == "" or args.force_print)
 writer.print_ascii_art(word, symbol=symbol, output_file=output, verbose=verbose)
 
-if output != "":
+if not verbose:
     print(f"The phrase { word } has been written in the file {output} using the symbol '{symbol}'.")
